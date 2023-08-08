6ea8fed0a40d0565696aa2dea37d592eb7812f0d
Mitchellzhou1 <mitche.zhou1@gmail.com>
2023-08-07 21:03:39 -0400

    RESTART

---
+++ SQL/Inserts.sql
@@ -0,0 +1,8 @@
+insert into users (email, password, name, phone, privilege) VALUES 
+("edward@gmail.com", "Suii", " Edward", "103-383-9910", "Student");
+
+insert into users VALUES
+('CR7@gmail.com', 'G%I44pHZ63QF*', 'admin', '(555) 749 9103', 'admin');
+
+insert into users
+values ('guestuser@gmail.com', '04wQQY!^49c*', 'user', '(555) 394 3421', 'user');
--- login.py
+++ login.py
@@ -1,4 +1,11 @@
 from app import *
+import re
+ 
+  
+def validEmail(email):
+    regex = "^[a-zA-Z0-9-_!#$%&'*+-/=?^_`{|}~']+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+    return re.search(regex, email)
+
     
 
 
@@ -9,40 +16,34 @@ def login(email = '', password = ''):
 
     if not email and not password:
         return render_template('login.html')
+    
+    message = "You have entered an invalid email"
+    if validEmail(email):
+        query = "SELECT * FROM users WHERE email = '" + email + "' AND password = %s"
+        cursor.execute(query, (password))
 
-    query = "SELECT * FROM users WHERE email = %s AND password = %s"
-    cursor.execute(query, (email, password))
+        user = cursor.fetchone()
+        if user:
+            session['email'] = email
+            return render_template('userHome.html', name = user['name'], email=user['email'], phone=user['phone'])
+        message = "Sorry, this user is doesn't exist"
 
-    user = cursor.fetchone()
     cursor.close()
-    if user:
-        session['email'] = email
-        session['ID'] = user['userID']
-        return render_template('userHome.html', name = user['name'], email=user['email'], ID=user['userID'])
-    message = "Sorry, the creditials are incorrect"
-
     return render_template('login.html', error=message)
 
 
 
-@app.route('/home', methods = ["POST"])
+@app.route('/submit', methods = ["POST"])
 def submit():
     email = request.form['email']
     password = request.form['password']
     return login(email, password)
 
 
-@app.route('/home/{{userID}}', methods = ["POST"])
-def getUser(userID):
-    cursor = conn.cursor()
-    session['ID'] = userID
-    query = "SELECT * FROM users WHERE userID = %s"
-    cursor.execute(query, (userID))
-
-    user = cursor.fetchone()
-
-    if user:
-        return render_template('userHome.html', name = user['name'], email=user['email'], ID=user['userID'])
-    else:
-        message = "Sorry, that person doesn't exist in our database"
+@app.route('/userHome', methods = ["POST"])
+def openHomePage():
+    try:
+        email = session['email']
+    except Exception:
+        message = 'Please Login or Create an Account'
         return render_template('login.html', error=message)
